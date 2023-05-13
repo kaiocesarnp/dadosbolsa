48b8215843efa9514bd8f160f10b0612ede83e7f
kaiocesarnp <kaiocesarnp@gmail.com>
2023-05-12 22:50:57 -0300

    dadosbolsa

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas_datareader as pdr
+
+# Define o símbolo da ação que você quer baixar os dados
+symbol = 'PETR3.SA'
+
+# Define o intervalo de data que você quer baixar os dados
+start_date = '2021-01-04'
+end_date = '2021-04-20'
